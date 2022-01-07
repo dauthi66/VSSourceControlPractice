5c6db55b26e121e30518fac2233a77b85cb71a99
Kris <dauthi66@gmail.com>
2022-01-07 14:04:41 -0800

    Added a real toe-tapping song.

--- VSSourceControlPractice/Program.cs
+++ VSSourceControlPractice/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! It's Raining Tacos!");
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Tacos\n" +
+                        "No need to ask why\n" +
+                        "Just open your mouth and close your eyes\n" +
+                        "It's raining tacos\n");
+}
